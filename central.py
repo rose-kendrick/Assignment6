fixed the function and the boundry calculations for the derivative

central.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as pp
 
 
 def f(x):
-    y = 2+0.75*np.tan(2*x)
+    y = 2+0.75*np.tanh(2*x)
     return y
 
 def f_prime(x,dx):
@@ -11,7 +11,7 @@ def f_prime(x,dx):
     return y
 
 def realfPrime(x):
-    y = 1.5/(np.cos(2*x))**2 
+    y = 1.5/(np.cosh(2*x))**2 
     return y
 
 dx = [1,0.5,0.1]
@@ -22,6 +22,9 @@ for n in range(len(dx)):
     for i in range(len(x)):
         fP = f_prime(x[i],dx[n])
         fPrime.append(fP)
+
+    fPrime[0] = 0
+    fPrime[-1] = 0
 
     pp.figure()
     pp.plot(x,f(x),color='blue')